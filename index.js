4f45bb2af3ee71a1ab2d9f3d54f1158df565c773
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-29 08:54:06 -0700

    step five, implemented a complex track for npc to walk along
    commented out line 8 for complex track to work successfully

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+// npc.walkEast()
 
 // Create the inventory
 const inventory = newInventory()
@@ -21,7 +21,17 @@ move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
 
-// Step 3: Testing npc.walkEast
-npc.walkEast(500, () => {
-    npc.walkNorth()    
+npc.walkNorth(1400, () => {
+    npc.walkEast(1200, () => {
+        npc.walkSouth(300, () => {
+            npc.walkEast(1500, () => {
+                npc.walkSouth(1500, () => {
+                    npc.walkWest(2700, () => {
+                        npc.walkNorth(400, () => {
+                        })
+                    })
+                })
+            })
+        })
+    })
 })

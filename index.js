cc3d57c9e1c1c45128a9b344ba03ace0479f1f04
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-29 17:01:34 -0700

    bonus, added collisionDetected function and while loop to check for collisions
    need to debug more as the code to optimize

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ npc.walkNorth(1400, () => {
     })
 })
 
-// Bonus: Testing 'Detect Collisions'
 /* 
+// Bonus: Testing 'Detect Collisions'
+
     Collision Logic: 
     Player disappears when a collision is made with npc
     if () remove player with .remove() ? do nothing
@@ -54,4 +55,23 @@ npc.walkNorth(1400, () => {
         // Find an element with a src that contains a string within that attribute and remove it
         document.querySelector('[src*="green-character"]').remove()
     }
+
+// Set a flag and while loop to run functions until a collision is detected
+let noCollisionsDetected = true;
+while(noCollisionsDetected) {
+    collisionDetected()
+}
+
+// Function to detect collisions, change flag to false when collision is detected
+function collisionDetected() {
+    if (
+        pc.x < npc.x + npc.w &&
+        pc.x + pc.w > npc.x &&
+        pc.y < npc.y + npc.h &&
+        pc.h + pc.y > npc.y
+        ) {
+            document.querySelector('[src*="green-character"]').remove()
+            return noCollisionsDetected = false;
+        }
+}
 */
